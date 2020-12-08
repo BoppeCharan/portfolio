839f9a110b3cae074e819d60a29a7c2764f44f5e
shubhraagarwal <agarwalshubhra2002@gmail.com>
2020-12-09 01:57:17 +0530

    Progress

---
+++ js/app.js
@@ -0,0 +1,17 @@
+ function handleMouseOver() {
+        document.getElementById('h12').style.color = 'white';
+        document.getElementById('h11').style.color = 'white';
+ }
+  function handleMouseOut() {
+        document.getElementById('h12').style.color = '#02D463';
+        document.getElementById('h11').style.color = '#02D463';
+ }
+
+  function handleMouseOver1() {
+        document.getElementById('h14').style.color = 'white';
+        document.getElementById('h13').style.color = 'white';
+ }
+  function handleMouseOut1() {
+        document.getElementById('h14').style.color = '#02D463';
+        document.getElementById('h13').style.color = '#02D463';
+ }
