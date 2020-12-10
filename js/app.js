7f1c62e968db4279e08017f356b1e067dbea70df
shubhraagarwal <agarwalshubhra2002@gmail.com>
2020-12-10 15:28:47 +0530

    Almost Complete

--- js/app.js
+++ js/app.js
@@ -23,3 +23,12 @@
         document.getElementById('h15').style.color = '#02D463';
         document.getElementById('h16').style.color = '#02D463';
  }
+
+   function handleMouseOver3() {
+        document.getElementById('h17').style.color = 'white';
+        document.getElementById('h18').style.color = 'white';
+ }
+  function handleMouseOut3() {
+        document.getElementById('h17').style.color = '#02D463';
+        document.getElementById('h18').style.color = '#02D463';
+ }
